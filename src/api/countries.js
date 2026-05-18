2449219fe943732ae6bc8d3b8d18039d22de0d8f
Nissa Saldivar <nissasaldivar@Nissas-MacBook-Air.local>
2026-05-18 18:10:34 -0500

    minor changes to cacheing

--- src/api/countries.js
+++ src/api/countries.js
@@ -1,45 +1,24 @@
 const baseURL = 'https://restcountries.com/v3.1/region'
 
-const DEFAULT_TTL_MS = 30 * 60 * 1000
+const cache = {} 
 
-const resolvedCache = new Map()
-const inFlight = new Map(
-)
-
-/** get json data w/ inflight dedupe and a short in-memory ttl */
-
-function cacheJSON(url, ttlMS = DEFAULT_TTL_MS) {
-    const currentTime = Date.now()
-    const resolved = resolvedCache.get(url)
-    if (resolved && resolved.expiresAt > currentTime) {
-        return Promise.resolve(resolved.data)
-    }
-
-    const pending = inFlight.get(url)
-    if (pending) return pending
-
-    const promise = fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            resolvedCache.set(url, {data, expiresAt: Date.now() + ttlMS, })
-            inFlight.delete(url)
-            return data
-            }) 
-        .catch((err) => {
-            inFlight.delete(url)
-            throw err
+function cacheJSON(url) {
+    if (cache[url]) {
+        return Promise.resolve(cache[url])
+    } else { 
+       return fetch(url)
+        .then(function(res) {
+            return res.json()
         })
-
-        inFlight.set(url, promise)
-        return promise
-
+        .then(function(data) {
+            cache[url] = data
+            return data
+        })
+    }
 }
 
-/** remove all cached responses & pending dedupe entries so next fetch will get a new request */
-
 export function clearCountriesCache() {
-    resolvedCache.clear()
-    inFlight.clear()
+    Object.keys(cache).forEach((key) => delete cache[key])
 }
 
 
